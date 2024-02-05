d6be5980cc770b777eec09e7b66d5c727a64bd43
Jkatu <141175580+Jkatu@users.noreply.github.com>
2024-02-05 19:15:43 +0300

    Add files via upload

---
+++ mavenproject8/src/main/java/com/katuleve/mavenproject8/Mavenproject8.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katuleve.mavenproject8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rhoda
+ */
+public class Mavenproject8 {
+
+    public static void main(String[] args) {
+        //initialise variables
+        int Base=10, Height=7,Hypotenuse=12 ;
+        //Give the formula of area and perimeter
+       double  area =  (0.5*Base*Height);
+        double  perimeter =  (Base + Height + Hypotenuse );
+        //give the output
+       
+        System.out.println("the area of the triangle is" + area);
+        System.out.println("the perimeter of the triangle is" + perimeter);
+        
+    }
+}
